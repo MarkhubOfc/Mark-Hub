6441a1004f7d78223227cacbafb862e0f10e09a0
MarkhubOfc <mark.hub.ofc@gmail.com>
2026-04-27 08:48:38 -0300

    feat: setup inicial Mark Hub

---
+++ theme.config.jsx
@@ -0,0 +1,10 @@
+export default {
+  logo: <strong>Mark Hub</strong>,
+  project: {
+    link: 'https://github.com/SEU_USUARIO/mark-hub',
+  },
+  darkMode: true,
+  footer: {
+    text: '© 2026 Mark Hub',
+  },
+}
