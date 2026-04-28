6732813447ab380c90e8243d33f4b82951a911cd
MarkhubOfc <mark.hub.ofc@gmail.com>
2026-04-28 17:54:43 -0300

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -1,1 +1,11 @@
+local success, GameList = pcall(function()
+  return loadstring(game:HttpGet("https://raw.githubusercontent.com/MarkhubOfc/Mark-Hub/refs/heads/main/List.lua"))()
+end)
 
+if success and GameList then
+  local currentId = tostring(game.PlaceId)
+  if GameList[currentId] then
+    loadstring(game:HttpGet(GameList[currentId]))()
+    return
+  end
+end
