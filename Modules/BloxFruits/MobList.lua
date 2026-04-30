e264598173301421520c4bb53a80ec875030fbf2
MarkhubOfc <mark.hub.ofc@gmail.com>
2026-04-30 11:55:08 -0300

    Update MobList.lua

--- Modules/BloxFruits/MobList.lua
+++ Modules/BloxFruits/MobList.lua
@@ -1,1 +1,25 @@
+local List = {}
 
+function List:CheckLevel()
+  local MyLevel = game:GetService("Players").LocalPlayer.Data.Level.Value
+  
+  if MyLevel >= 1 and MyLevel <= 9 then
+    return {
+      QuestName = "BanditQuest1",
+      QuestLvl = 1,
+      EnemyName = "Bandit",
+      fPos = CFrame.new(1228, 63, 1515),
+      QuestPos = CFrame.new(1063, 17, 1547)
+    }
+  elseif MyLevel >= 10 and MyLevel <= 14 then
+    return {
+      QuestName = "JungleQuest",
+      QuestLvl = 1,
+      EnemyName = "Monkey",
+      fPos = CFrame.new(-1448, 48, 58),
+      QuestPos = CFrame.new(-1603, 37, 153)
+    }
+  end
+end
+
+return List
