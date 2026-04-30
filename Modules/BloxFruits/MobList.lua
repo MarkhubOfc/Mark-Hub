d0c70c38b1f4d832fa66d8c44b5201e6f492b1b7
MarkhubOfc <mark.hub.ofc@gmail.com>
2026-04-30 12:28:52 -0300

    Update MobList.lua

--- Modules/BloxFruits/MobList.lua
+++ Modules/BloxFruits/MobList.lua
@@ -1,37 +1,38 @@
 local List = {}
 
 function List:CheckLevel()
-  local MyLevel = game:GetService("Players").LocalPlayer.Data.Level.Value
+  local lp = game:GetService("Players").LocalPlayer
+  local MyLevel = lp.Data.Level.Value
   
   if MyLevel >= 1 and MyLevel <= 9 then
     return {
       QuestName = "BanditQuest1",
-      QuestLvl = 1,
       EnemyName = "Bandit",
+      QuestLvl = 1,
       fPos = CFrame.new(1228, 63, 1515),
       QuestPos = CFrame.new(1063, 17, 1547)
     }
   elseif MyLevel >= 10 and MyLevel <= 14 then
     return {
       QuestName = "JungleQuest",
-      QuestLvl = 1,
       EnemyName = "Monkey",
+      QuestLvl = 1,
       fPos = CFrame.new(-1448, 48, 58),
       QuestPos = CFrame.new(-1603, 37, 153)
     }
   elseif MyLevel >= 15 and MyLevel <= 19 then
     return {
       QuestName = "JungleQuest",
-      QuestLvl = 2,
       EnemyName = "Gorilla",
+      QuestLvl = 2,
       fPos = CFrame.new(-1328, 19, -543),
       QuestPos = CFrame.new(-1603, 37, 153)
     }
   elseif MyLevel >= 20 then
     return {
       QuestName = "JungleQuest",
-      QuestLvl = 3,
       EnemyName = "The Gorilla King",
+      QuestLvl = 3,
       fPos = CFrame.new(-1328, 19, -543),
       QuestPos = CFrame.new(-1603, 37, 153)
     }
