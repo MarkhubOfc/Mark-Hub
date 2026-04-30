5ca78fed3b8fd17b3dad56ada53258fab466fce4
MarkhubOfc <mark.hub.ofc@gmail.com>
2026-04-30 12:30:29 -0300

    Update List.lua

--- List.lua
+++ List.lua
@@ -1,4 +1,4 @@
 return {
   ["142823291"] = "https://raw.githubusercontent.com/MarkhubOfc/Mark-Hub/refs/heads/main/Games/MM2.lua",
-  [""] = "https://raw.githubusercontent.com/MarkhubOfc/Mark-Hub/refs/heads/main/Games/"
+  [""] = "https://raw.githubusercontent.com/MarkhubOfc/Mark-Hub/refs/heads/main/Games/BloxFruits/Sea1.lua"
 }
