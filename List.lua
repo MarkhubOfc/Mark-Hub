93f7fc0b9536076b06f12253628608f5a98db5a1
MarkhubOfc <mark.hub.ofc@gmail.com>
2026-05-09 23:46:42 -0300

    Update List.lua

--- List.lua
+++ List.lua
@@ -1,4 +1,6 @@
 return {
   ["142823291"] = "https://raw.githubusercontent.com/MarkhubOfc/Mark-Hub/refs/heads/main/Games/MM2.lua", -- Murder Mystery 2
-  ["2753915549"] = "https://raw.githubusercontent.com/MarkhubOfc/Mark-Hub/refs/heads/main/Games/BloxFruits/Sea1.lua" -- Blox fruits ( sea 1 )
+  ["2753915549"] = "https://raw.githubusercontent.com/MarkhubOfc/Mark-Hub/refs/heads/main/Games/BloxFruits/Sea1.lua", -- Blox fruits ( sea 1 )
+  ["1"] = "https://raw.githubusercontent.com/MarkhubOfc/Mark-Hub/refs/heads/main/Games/BloxFruits/Sea2.lua", -- Blox fruits ( Sea 2 )
+  ["7449423635"] = "https://raw.githubusercontent.com/MarkhubOfc/Mark-Hub/refs/heads/main/Games/BloxFruits/Sea3.lua" -- Blox fruits ( Sea 3 )
 }
