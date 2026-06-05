9e066a7069cf1f713e0c65c0df76227e235defb2
Eu sou brasileiro <mark.hub.ofc@gmail.com>
2026-06-05 06:24:57 -0300

    Update List.lua

--- List.lua
+++ List.lua
@@ -1,5 +1,6 @@
 return {
   ["142823291"] = "https://raw.githubusercontent.com/MarkhubOfc/Mark-Hub/refs/heads/main/Games/MM2.lua", -- Murder Mystery 2
+  ["1"] = "https://raw.githubusercontent.com/Zynkore/Hub/refs/heads/main/Games/Flee%20The%20Facility.lua", -- Flee the facility
   ["2753915549"] = "https://raw.githubusercontent.com/MarkhubOfc/Mark-Hub/refs/heads/main/Games/BloxFruits/Sea1.lua", -- Blox fruits ( sea 1 )
   ["1"] = "https://raw.githubusercontent.com/MarkhubOfc/Mark-Hub/refs/heads/main/Games/BloxFruits/Sea2.lua", -- Blox fruits ( Sea 2 )
   ["7449423635"] = "https://raw.githubusercontent.com/MarkhubOfc/Mark-Hub/refs/heads/main/Games/BloxFruits/Sea3.lua" -- Blox fruits ( Sea 3 )
