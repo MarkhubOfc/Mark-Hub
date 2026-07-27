4cb040420d5f789185b2c7ede0e016259a8d8691
Eu sou brasileiro <mark.hub.ofc@gmail.com>
2026-07-27 13:52:40 -0300

    Test

--- Modules/BloxFruits/Module.lua
+++ Modules/BloxFruits/Module.lua
@@ -4,9 +4,8 @@ function Module:GetToken()
   local plr = (cloneref and cloneref(game:GetService('Players')) or game:GetService('Players'))
   local lp = plr.LocalPlayer
   local UserId = lp.UserId
-
-  local Token = 'ABC0xJbb0'
-
+  local Token = tostring(UserId):sub(2, 4) .. tostring(math.random(10000, 99999))
+  
   return Token
 end
 
